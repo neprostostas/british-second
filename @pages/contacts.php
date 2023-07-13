845b6a03ea845bbf9e69d81ba191e2c364fdb749
neprostostas <neprostostas.gg@gmail.com>
2023-07-14 00:13:33 +0300

    contacts page done

--- @pages/contacts.php
+++ @pages/contacts.php
@@ -1,1 +1,213 @@
-<div class="contacts">contacts</div>
+<main class="contacts-page">
+    <div class="hero-section contacts">
+
+        <div class="hero-wrapper">
+            <div class="hero-image contacts"></div>
+
+            <div class="hero-text contacts">
+
+                <div class="top-line">
+
+                    <hr class="orange">
+
+                    <h2>Оберіть один з 3-х варіантів зв'язку з BritishSecond</h2>
+
+                </div>
+
+                <div class="second-line">
+
+                    <h4 class="code orange">1</h4>
+
+                    <p>Залиште своє ім’я та номер телефону у формі нижче та ми швидко вам зателефонуємо</p>
+
+                </div>
+
+                <form class="contacts-form">
+
+                    <div class="name-field">
+                        <label for="name"></label>
+                        <input required placeholder="ВАШЕ ІМ'Я" type="text" id="name" name="name" minlength="2">
+                    </div>
+
+                    <div class="number-field">
+                        <label for="number"></label>
+                        <input required placeholder="ВАШ НОМЕР ТЕЛЕФОНУ" type="number" id="number" name="number" min="10" max="100">
+                    </div>
+
+                    <button class="contact" type="submit" value="Submit">
+                        <span>Замовити дзвінок</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="41" height="11" viewBox="0 0 41 11" fill="none">
+                            <path d="M0.545898 5.47296H39.7561M39.7561 5.47296L35.9493 0.405396M39.7561 5.47296L35.9493 10.5405" stroke="black"/>
+                        </svg>
+                    </button>
+
+                </form>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="container">
+
+        <div class="contact-section">
+
+            <div class="left-part">
+                <h4 class="code black">2</h4>
+                <p>Оберіть зручного вам оператора мобільного зв'язку чи надішліть е-mail</p>
+            </div>
+
+            <div class="right-part">
+
+                <div class="numbers-wrap">
+                    <img src="<?= SERVER_URL ?>assets/images/icons/call.svg" alt="call">
+                    <div class="numbers">
+                        <a class="text-call" href="tel:+380973841792">(097) 384-17-92</a>
+                        <a class="text-call" href="tel:+380637881118">(063) 788-11-18</a>
+                    </div>
+                </div>
+
+                <div class="email-wrap">
+                    <img src="<?= SERVER_URL ?>assets/images/icons/mail.svg" alt="mail">
+                    <a class="text-mail" href="mailto:britishsecond@gmail.com">britishsecond@gmail.com</a>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="container">
+
+        <div class="manager-section">
+
+            <div class="top-line">
+                <h4 class="code orange">3</h4>
+                <p>З вами працюють досвідчені менеджери</p>
+            </div>
+
+            <div class="content-line">
+
+                <div class="author-box one">
+
+                    <img src="<?= SERVER_URL ?>assets/images/contacts/authors-1.png" alt="authors-1">
+
+                    <div class="bottom-part">
+
+                        <div class="info-line">
+                            <p>менеджер з продажу</p>
+                            <h3>Олександр Корнюк</h3>
+                        </div>
+
+                        <div class="contact-line">
+
+                            <div class="call-part">
+                                <img src="<?= SERVER_URL ?>assets/images/icons/call.svg" alt="call">
+                                <a class="text-call" href="tel:+380954544456">+38 (095) 454-44-56</a>
+                            </div>
+                            <div class="skype-part">
+                                <img src="<?= SERVER_URL ?>assets/images/icons/skype.svg" alt="skype">
+                                <a class="text-skype" href="skype:olexander.lornyk?userinfo">olexander.lornyk</a>
+                            </div>
+                            <div class="mail-part">
+                                <img src="<?= SERVER_URL ?>assets/images/icons/mail.svg" alt="mail">
+                                <a class="text-mail" href="mailto:olexander.lornyk@gmail.com">olexander.lornyk@gmail.com</a>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="author-box two">
+
+                    <img src="<?= SERVER_URL ?>assets/images/contacts/authors-2.png" alt="authors-2">
+
+                    <div class="bottom-part">
+
+                        <div class="info-line">
+                            <p>менеджер з продажу</p>
+                            <h3>Олександр Корнюк</h3>
+                        </div>
+
+                        <div class="contact-line">
+
+                            <div class="call-part">
+                                <img src="<?= SERVER_URL ?>assets/images/icons/call.svg" alt="call">
+                                <a class="text-call" href="tel:+380954544456">+38 (095) 454-44-56</a>
+                            </div>
+                            <div class="skype-part">
+                                <img src="<?= SERVER_URL ?>assets/images/icons/skype.svg" alt="skype">
+                                <a class="text-skype" href="skype:olexander.lornyk?userinfo">olexander.lornyk</a>
+                            </div>
+                            <div class="mail-part">
+                                <img src="<?= SERVER_URL ?>assets/images/icons/mail.svg" alt="mail">
+                                <a class="text-mail" href="mailto:olexander.lornyk@gmail.com">olexander.lornyk@gmail.com</a>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="author-box three">
+
+                    <img src="<?= SERVER_URL ?>assets/images/contacts/authors-3.png" alt="authors-3">
+
+                    <div class="bottom-part">
+
+                        <div class="info-line">
+                            <p>менеджер з продажу</p>
+                            <h3>Олександр Корнюк</h3>
+                        </div>
+
+                        <div class="contact-line">
+
+                            <div class="call-part">
+                                <img src="<?= SERVER_URL ?>assets/images/icons/call.svg" alt="call">
+                                <a class="text-call" href="tel:+380954544456">+38 (095) 454-44-56</a>
+                            </div>
+                            <div class="skype-part">
+                                <img src="<?= SERVER_URL ?>assets/images/icons/skype.svg" alt="skype">
+                                <a class="text-skype" href="skype:olexander.lornyk?userinfo">olexander.lornyk</a>
+                            </div>
+                            <div class="mail-part">
+                                <img src="<?= SERVER_URL ?>assets/images/icons/mail.svg" alt="mail">
+                                <a class="text-mail" href="mailto:olexander.lornyk@gmail.com">olexander.lornyk@gmail.com</a>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="container">
+
+        <div class="storage-section">
+
+            <div class="top-line">
+
+                <hr class="orange">
+
+                <h2>Інформація для візиту на склад</h2>
+
+                <hr class="orange">
+
+            </div>
+
+        </div>
+
+    </div>
+
+</main>
