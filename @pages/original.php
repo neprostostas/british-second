8f2ce3aa203db0697eb0f6942e06a34069f9e61b
neprostostas <neprostostas.gg@gmail.com>
2023-07-13 19:38:31 +0300

    original page done

--- @pages/original.php
+++ @pages/original.php
@@ -1,1 +1,498 @@
-<div class="original">original</div>
+<main class="original-page">
+
+    <div class="heroline-wrap">
+
+        <div class="hero-line">
+            <img src="<?= SERVER_URL ?>assets/images/icons/flag-1.svg" alt="flag-1">
+            <img src="<?= SERVER_URL ?>assets/images/icons/flag-2.svg" alt="flag-2">
+        </div>
+
+        <div class="hero-section original">
+
+            <div class="hero-wrapper">
+                <div class="hero-image original"></div>
+
+                <div class="hero-text original">
+                    <h2>Замовте оригінал особливої збірки</h2>
+
+                    <p>з багатих районів Англії та Шотландії</p>
+
+                    <button class="party">
+                        Обрати партію
+                        <svg xmlns="http://www.w3.org/2000/svg" width="53" height="12" viewBox="0 0 53 12" fill="none">
+                            <path d="M0 6H51.5M51.5 6L46.5 1M51.5 6L46.5 11" stroke="#DF042E"/>
+                        </svg>
+                    </button>
+                </div>
+
+            </div>
+
+        </div>
+        <div class="container">
+
+            <div class="interesting-section">
+
+                <div class="wrap-orange">
+                    <div class="orange-block">
+
+                        <p>Кожен власник діючого бізнесу на секонд-хенді знає, як важко знайти надійного постачальника.
+                            Якщо не сподіватись на фортуну, то на цьому шляху є маса підводних каменів. </p>
+
+                        <hr class="white">
+
+                        <hr class="black">
+
+                    </div>
+                    <div class="img-wrap">
+                        <img class="img-original-1" src="<?= SERVER_URL ?>assets/images/original/original-1.png" alt="original-1">
+                    </div>
+                </div>
+
+                <div class="point-block">
+
+                    <p class="bold">Одні з найчастіших, про які варто задуматись як тим, хто вже має магазин так і тим, хто лише планує почати бізнес, це:</p>
+
+                    <div class="line-num">
+                        <div class="block-number">1</div>
+                        <p>Наявність у постачальника власних магазинів. В більшості випадків на складі з партії товару можуть щезнути речі високої якості</p>
+                    </div>
+
+                    <div class="line-num">
+                        <div class="block-number">2</div>
+                        <p>Завдаток за доставку. В цьому разі, якщо товар вас не влаштував, не співпав по вазі і ви його повернули, ви вже в мінусі на сплачену суму</p>
+                    </div>
+
+                    <div class="line-num">
+                        <div class="block-number">3</div>
+                        <p>Сумнівна якість та збірка. Більшість постачальників закупають товар на фабриках, які займаються контейнерною збіркою. Отримавши такий секонд-хенд, ви до останнього не знатимете який % браку і неліквіду виявите при огляді.</p>
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="container">
+
+        <div class="facts-section">
+
+            <div class="fact-block">
+                <div class="image-wrap">
+                    <hr class="orange">
+                    <img class="img-interesting-2" src="<?= SERVER_URL ?>assets/images/original/original-2.png" alt="original-2">
+                </div>
+                <div class="text-part">
+                    <p>Ок, скажете ви - чому ж не замовляти власні партії напряму з Європи? Так, це цілком можливо, але вам доведеться об’їхати декілька фабрик, обрати ту, з якою співпрацюватимете та, головне, зуміти продавати ті щомісячні обсяги, які вони вкажуть у контракті. </p>
+                    <p class="bold">Висновок - цей варіант для власників великих мереж магазинів. А невдалий вибір фабрики призведе до отримання партій з високим вмістом “сміття” - бракованого та неліквідного товару.</p>
+                </div>
+            </div>
+
+            <div class="last-block">
+                <hr class="orange thin">
+                <p>BritishSecond вирішує всі ці проблеми!</p>
+            </div>
+
+            <button class="choose-item">
+                <span>Перейти до вибору товару</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="53" height="12" viewBox="0 0 53 12" fill="none">
+                    <path d="M0 6H51.5M51.5 6L46.5 1M51.5 6L46.5 11" stroke="black"/>
+                </svg>
+            </button>
+
+        </div>
+
+    </div>
+
+    <div class="container">
+
+        <div class="sell-section">
+
+            <h2>Подивіться товари, які є в продажу</h2>
+
+            <div class="sell-wrap">
+
+                <div class="sell-box one">
+
+                    <div class="image-bg"></div>
+
+                    <div class="content-wrap">
+
+                        <div class="content-block">
+
+                            <div class="top-part">
+
+                                <hr class="orange top">
+
+                                <h3>Dor2Dor</h3>
+
+                                <hr class="orange bot">
+
+                                <p>Речі збірки напряму з шотландських домівок</p>
+
+                                <p class="grey">Ленточка м.Глазго</p>
+
+                            </div>
+
+                            <div class="bottom-part">
+                                <h4>2,82 <span>$</span></h4>
+
+                                <button class="specify">
+                                    <span>Уточнити</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="27" height="12" viewBox="0 0 27 12" fill="none">
+                                        <path d="M0 6H25.5M25.5 6L20.5 1M25.5 6L20.5 11" stroke="black"/>
+                                    </svg>
+                                </button>
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="sell-box two">
+
+                    <div class="image-bg"></div>
+
+                    <div class="content-wrap">
+
+                        <div class="content-block">
+
+                            <div class="top-part">
+
+                                <hr class="orange top">
+
+                                <h3>Очищений оригінал</h3>
+
+                                <hr class="orange bot">
+
+                                <p>Перебраний в Європі, зі 100% речей залишається лише 37%,тобто 63% гіршого товару не попадає до вас</p>
+
+                            </div>
+
+                            <div class="bottom-part">
+
+                                <h4>5,20 <span>$</span></h4>
+
+                                <button class="specify">
+                                    <span>Уточнити</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="27" height="12" viewBox="0 0 27 12" fill="none">
+                                        <path d="M0 6H25.5M25.5 6L20.5 1M25.5 6L20.5 11" stroke="black"/>
+                                    </svg>
+                                </button>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="sell-box three">
+
+                    <div class="image-bg"></div>
+
+                    <div class="content-wrap">
+
+                        <div class="content-block">
+
+                            <div class="top-part">
+
+                                <hr class="orange top">
+
+                                <h3>Cash4Clothes</h3>
+
+                                <hr class="orange bot">
+
+                                <p>Це магазинний збір з контролем якості кожної речі.</p>
+
+                                <p class="grey">Маріус - одяг, взуття, сумки, біжутерія</p>
+
+                            </div>
+
+                            <div class="bottom-part">
+
+                                <h4>2,57 <span>$</span></h4>
+
+                                <button class="specify">
+                                    <span>Уточнити</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="27" height="12" viewBox="0 0 27 12" fill="none">
+                                        <path d="M0 6H25.5M25.5 6L20.5 1M25.5 6L20.5 11" stroke="black"/>
+                                    </svg>
+                                </button>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+                <div class="sell-box four">
+
+                    <div class="image-bg"></div>
+
+                    <div class="content-wrap">
+
+                        <div class="content-block">
+
+                            <div class="top-part">
+
+                                <hr class="orange top">
+
+                                <h3>Dor2Dor</h3>
+
+                                <hr class="orange bot">
+
+                                <p>Речі збірки напряму з британських домівок</p>
+
+                                <p class="grey">Ленточка м.Брістоль</p>
+
+                            </div>
+
+                            <div class="bottom-part">
+
+                                <h4>2,47 <span>$</span></h4>
+
+                                <button class="specify">
+                                    <span>Уточнити</span>
+                                    <svg xmlns="http://www.w3.org/2000/svg" width="27" height="12" viewBox="0 0 27 12" fill="none">
+                                        <path d="M0 6H25.5M25.5 6L20.5 1M25.5 6L20.5 11" stroke="black"/>
+                                    </svg>
+                                </button>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="container">
+        <div class="viber-section">
+            <div class="viber-block">
+                <hr class="white medium">
+                <h2>
+                    Знижки для постійних клієнтів
+                </h2>
+                <p>Для тих, хто замовляє партію від 5 тон передбачена система знижок</p>
+                <button class="detail">
+                    <span>Дізнатись подробиці</span>
+                    <svg xmlns="http://www.w3.org/2000/svg" width="53" height="12" viewBox="0 0 53 12" fill="none">
+                        <path d="M0 6H51.5M51.5 6L46.5 1M51.5 6L46.5 11" stroke="black"/>
+                    </svg>
+                </button>
+            </div>
+            <div class="viber-image">
+                <img src="<?= SERVER_URL ?>assets/images/original/discount-1.png" alt="discount-1">
+                <hr class="black medium">
+                <hr class="black thin">
+            </div>
+        </div>
+    </div>
+
+    <div class="container">
+
+        <div class="benefit-section">
+
+            <div class="top-line">
+                <h2>Основні переваги</h2>
+                <p>співпраці з BritishSecond</p>
+            </div>
+
+            <div class="main-line">
+
+                <div class="padding-mobile orange">
+                    <div class="benefit-block one orange">
+
+                        <div class="image-wrapper">
+                            <hr class="black">
+                            <img src="<?= SERVER_URL ?>assets/images/original/adv-1.png" alt="adv-1">
+                        </div>
+
+                        <div class="content-wrapper">
+                            <h3>Повна відсутність передплати</h3>
+
+                            <hr class="black">
+
+                            <p>Отже ви можете бути впевнені, що товар, який приїхав на склад, і товар, який ви купили, - ідентичні.</p>
+                            <p>Для більшої впевненості ми найняли окремого працівника, який робить відеоогляди і наполягаємо, щоб ви завітали до нас, як мінімум при першій покупці.</p>
+
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="padding-mobile grey">
+                    <div class="benefit-block two grey">
+
+                        <div class="content-wrapper">
+                            <h3>Відсутність завдатків і передплат</h3>
+
+                            <hr class="black">
+
+                            <p>Ми роками співпрацюємо лише з тими фабриками, які перевірили і яким довіряємо. Репутація - наш пріоритет.</p>
+                            <p>До того ж, ми не прагнемо заробити на невеличких партіях (і втратити вашу довіру) - нам цікаві довготривалі и прозорі стосунки.</p>
+
+                        </div>
+
+                        <div class="image-wrapper">
+                            <hr class="black">
+                            <img src="<?= SERVER_URL ?>assets/images/original/adv-2.png" alt="adv-2">
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="padding-mobile orange">
+                    <div class="benefit-block three orange">
+
+                        <div class="image-wrapper">
+                            <hr class="orange">
+                            <img src="<?= SERVER_URL ?>assets/images/original/adv-3.png" alt="adv-3">
+                        </div>
+
+                        <div class="content-wrapper">
+                            <h3>Завжди приємні ціни і висока якість</h3>
+
+                            <hr class="black">
+
+                            <p>Хоча British Second веде прямий імпорт з Європи і співпрацює лише з ретельно відібраними фабриками, ми запровадили відеозйомки в Viber-спільноті, після яких ви чітко розумієте який товар, в якій кількості і якості замовляєте.</p>
+                            <p>Більше того, часто заплативши за 1 кг дорожче, ніж у нас, ви отримуєте товар на нижній межі якості категорії. Отже переплачуєте вдвічі і ризикуєте забити склад неліквідом.</p>
+                            <p>Ми працюємо з “домашньою збіркою” - це одяг та взуття, які віддають на благочинність, часто випрані, в ідеальному стані або нові з бірками. Враховуючи локації збору, в нашій домашній збірці багато брендового якісного товару.</p>
+
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="padding-mobile grey">
+                    <div class="benefit-block four grey">
+
+                        <div class="content-wrapper">
+                            <h3>2 варіанти доставки на ваш вибір</h3>
+
+                            <hr class="black">
+
+                            <div class="number-line">
+
+                                <hr class="black">
+
+                                <p>Обраним вами поштовим перевізником накладним платежем</p>
+
+                                <div class="number">1</div>
+
+                            </div>
+
+                            <div class="number-line">
+
+                                <hr class="black">
+
+                                <p>Нашим власним транспортом під двері магазину (діє в межах Західної та Центральної України, Східна Україна по індивідуальній замовленості)</p>
+
+                                <div class="number">2</div>
+
+                            </div>
+
+                            <div class="label-block">
+                                <h4>Порівняння цін на доставку:</h4>
+
+                                <div class="comparison-block">
+
+                                    <div class="product">
+                                        <img src="<?= SERVER_URL ?>assets/images/main/comparison-1.png" alt="comparison-1">
+                                        <p>British Second</p>
+                                        <h3 class="white">~ <span>1.5</span> грн/км</h3>
+                                    </div>
+
+                                    <hr class="grey">
+
+                                    <div class="product">
+                                        <img src="<?= SERVER_URL ?>assets/images/main/comparison-2.png" alt="comparison-2">
+                                        <p>Meest Express</p>
+                                        <h3>~ <span>4</span> грн/км</h3>
+                                    </div>
+
+                                    <hr class="grey">
+
+                                    <div class="product">
+                                        <img src="<?= SERVER_URL ?>assets/images/main/comparison-3.png" alt="comparison-3">
+                                        <p>Нова Пошта</p>
+                                        <h3>~ <span>6</span> грн/км</h3>
+                                    </div>
+
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div class="image-wrapper">
+                            <hr class="black">
+                            <img src="<?= SERVER_URL ?>assets/images/original/adv-4.png" alt="adv-4">
+                        </div>
+
+                    </div>
+                </div>
+
+            </div>
+
+            <div class="last-block">
+                <hr class="orange thin">
+                <p>Це лише декілька фактів про нас, якими ми ділимось, тому що на власному досвіді переконались, наскільки їх цінять наші клієнти</p>
+            </div>
+
+            <button class="choose-item">
+                <span>Перейти до вибору товару</span>
+                <svg xmlns="http://www.w3.org/2000/svg" width="53" height="12" viewBox="0 0 53 12" fill="none">
+                    <path d="M0 6H51.5M51.5 6L46.5 1M51.5 6L46.5 11" stroke="#F1AE2C"/>
+                </svg>
+            </button>
+
+        </div>
+
+    </div>
+
+    <div class="container">
+
+        <div class="document-section">
+
+            <div class="top-line">
+
+                <hr class="black">
+
+                <h2>Офіційно розмитнюємо та надаємо всі необхідні сертифікати</h2>
+
+            </div>
+
+            <div class="content-block">
+
+                <div class="left-part">
+
+                    <p class="padding-t">Вживаний одяг, повинен мати супровідну документацію - на кожну партію секонд-хенду країна, з якої імпортується продукція, видає документ про проведення дезінфекції, а також інші сертифікати.</p>
+                    <p class="padding-tb">Ці документи перевізник, який транспортує товар, повинен пред’явити під час проходження митного контролю.</p>
+                    <p class="padding-b">Такий документ видається на кожну партію продукції.</p>
+                    <p>Ваші покупці задля власної безпеки мають право ознайомитися з сертифікатами, тому ми надаємо нашим клієнтам все необхідне для легального та безпечного бізнесу</p>
+
+                </div>
+
+                <div class="slider placeholder-right"></div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</main>
