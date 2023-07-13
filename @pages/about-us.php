2d926d56c330e2225bf689073ad435ed3c1967bf
neprostostas <neprostostas.gg@gmail.com>
2023-07-13 21:29:56 +0300

    about us page done

--- @pages/about-us.php
+++ @pages/about-us.php
@@ -1,1 +1,167 @@
-<div class="about-us">about-us</div>
+<main class="about-page">
+
+    <div class="hero-section main final">
+
+        <div class="hero-wrapper">
+            <div class="hero-image main"></div>
+
+            <div class="hero-text main">
+
+                <div class="left-part">
+                    <h2>British Second запрошує до співпрацi</h2>
+
+                    <p>
+                        Якщо Ви - підприємець, який шукає надійного партнера з регулярними поставками якісного одягу з Британії, Бельгії, Шотандії, Італії, Нідерландів - British Second - це те, що вам необхідно.
+                    </p>
+
+                    <button class="connect-manager">
+                        Детальніше
+                        <svg xmlns="http://www.w3.org/2000/svg" width="53" height="12" viewBox="0 0 53 12" fill="none">
+                            <path d="M0 6H51.5M51.5 6L46.5 1M51.5 6L46.5 11" stroke="#F1AE2C"/>
+                        </svg>
+                    </button>
+                </div>
+                <div class="right-part">
+                    <div class="flag-wrap">
+                        <img src="<?= SERVER_URL ?>assets/images/icons/flag-1.svg" alt="flag-1">
+                        <img src="<?= SERVER_URL ?>assets/images/icons/flag-2.svg" alt="flag-2">
+                        <img src="<?= SERVER_URL ?>assets/images/icons/flag-3.svg" alt="flag-3">
+                        <img src="<?= SERVER_URL ?>assets/images/icons/flag-4.svg" alt="flag-4">
+                        <img src="<?= SERVER_URL ?>assets/images/icons/flag-5.svg" alt="flag-5">
+                    </div>
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+    <div class="container">
+
+        <div class="business-section">
+
+            <div class="block1">
+
+                <div class="left-part">
+
+                    <h2>Пропонуємо готовий бізнес!</h2>
+
+                    <hr class="orange">
+
+                    <p>Шукаємо партнерів (гуртовий склад). Рекомендовано з обласних центрів у центральній та східній Україні.</p>
+
+                </div>
+
+                <div class="right-part">
+
+                    <hr class="orange">
+
+                    <h4>Франшиза</h4>
+
+                    <img src="<?= SERVER_URL ?>assets/images/about/block-1.png" alt="block-1">
+
+                </div>
+
+            </div>
+
+            <div class="block2">
+
+                <hr class="white">
+
+                <div class="line-text">
+
+                    <img src="<?= SERVER_URL ?>assets/images/icons/quotes.svg" alt="quotes">
+
+                    <p>Ми готові поділитися досвідом з Вами, надати пораду щодо бізнесу, організувати доставку товару в будь-який куточок України за допомогою власного транспорту або транспортним перевізником за вашим вибором.</p>
+
+                </div>
+
+            </div>
+
+            <div class="block3">
+
+                <hr class="black">
+
+                <div class="line-diploma">
+
+                    <img src="<?= SERVER_URL ?>assets/images/icons/diploma.svg" alt="diploma">
+
+                    <div class="p-wrap">
+                        <p>Весь наш товар пройшов сертифікацію від санітарно-епідеміологічної служби та Міністерства екології.</p>
+                        <p>На нашому складі ви можете купити будь-яку партію товару: від одного мішка - і аж до повної вантажівки!</p>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="block4">
+
+                <h2>Вітається</h2>
+
+                <div class="line-hello">
+
+                    <div class="block">
+                        <div class="block-number">✓</div>
+                        <p>приміщення для складу</p>
+                    </div>
+
+                    <div class="block">
+                        <div class="block-number">✓</div>
+                        <p>власний транспорт</p>
+                    </div>
+
+                </div>
+
+            </div>
+
+            <div class="block5">
+
+                <h2>Стартові інвестиції складають</h2>
+
+                <div class="price-block">
+
+                    <div class="top-part">
+                        <p>від</p>
+                        <h3>$5000</h3>
+                    </div>
+
+                    <hr class="white">
+
+                    <h4>на закупку товару</h4>
+
+                </div>
+
+            </div>
+
+            <div class="block6">
+
+                <div class="image-wrap one"></div>
+
+                <div class="image-wrap two">
+
+                    <p>За детальною інформацією звертайтесь</p>
+
+                    <div class="numbers-wrap">
+
+                        <img src="<?= SERVER_URL ?>assets/images/icons/call-blue.svg" alt="call-blue">
+
+                        <div class="numbers">
+
+                            <a class="text-call" href="tel:+380973841792">(097) 384-17-92</a>
+                            <a class="text-call" href="tel:+380637881118">(063) 788-11-18</a>
+
+                        </div>
+
+                    </div>
+
+                </div>
+
+            </div>
+
+        </div>
+
+    </div>
+
+</main>
