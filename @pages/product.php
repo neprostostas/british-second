fcd5b73da25fee0b909d62ee285cdf093d4667d4
neprostostas <neprostostas.gg@gmail.com>
2023-07-15 00:54:04 +0300

    site is done

--- @pages/product.php
+++ @pages/product.php
@@ -4,8 +4,285 @@
 
         <div class="product-section">
 
+            <div class="breadcrumbs">
+                <a href="<?= SERVER_URL ?>"><img src="<?= SERVER_URL ?>assets/images/icons/home.svg" alt="home"></a>
+                <p>></p>
+                <a href="<?= SERVER_URL ?>">Літо</a>
+                <p>></p>
+                <a href="<?= SERVER_URL ?>">Кросівки</a>
+            </div>
 
+            <div class="title-part">
+                <h2>Одяг крем міх Бельгія 5123</h2>
+            </div>
 
+            <div class="main-wrap">
+                <div class="top-part">
+                    <div class="media-block">
+
+                        <img class="big-picture" src="<?= SERVER_URL ?>assets/images/product/product-1.png" alt="product-1">
+
+                        <div class="mini-wrap">
+                            <img src="<?= SERVER_URL ?>assets/images/product/product-mini-1.png" alt="product-mini-1">
+                            <img src="<?= SERVER_URL ?>assets/images/product/product-mini-2.png" alt="product-mini-2">
+                            <img src="<?= SERVER_URL ?>assets/images/product/product-mini-3.png" alt="product-mini-3">
+                            <img src="<?= SERVER_URL ?>assets/images/product/product-mini-4.png" alt="product-mini-4">
+                        </div>
+
+                    </div>
+                    <div class="description-block">
+
+                        <h3 class="desc-top">ОПИС</h3>
+
+                        <p>Дитячі класичні балетки, модель виготовлена з натурального матеріалу (котон) як зверху, так і всередині, щільна структура тканини, круглий носик надає можливість комфортному розташуванню дитячих пальчиків, п’ятка чітко фіксує ніжку, защіпка-липучка пристосовує взуття до особливостей підйому Вашої дитини та полегшує процес взування.</p>
+                        <p>Стійка, гнучка ПВХ підошва. Всередині шкіряна устілка-амортизатор з профілактичним супінатором.</p>
+                        <p>Дане взуття ідеальне для щоденних прогулянок по місцевостях різноманітного рельєфу та подорожей, зручне в догляді та комфортне у носінні, стильне, яскраве!</p>
+
+                        <div class="price-wrap">
+                            <div class="price-block">
+
+                                <div class="line-top">
+                                    <img class="price-img" src="<?= SERVER_URL ?>assets/images/icons/price.svg" alt="price">
+                                    <p>Вартість</p>
+                                </div>
+
+                                <h3>5,5<span>$</span></h3>
+
+                            </div>
+                            <div class="price-block">
+
+                                <div class="line-top">
+                                    <img class="weight-img" src="<?= SERVER_URL ?>assets/images/icons/weight.svg" alt="weight">
+                                    <p>Вага</p>
+                                </div>
+
+                                <h3>25<span>кг</span></h3>
+
+                            </div>
+                            <div class="price-block">
+
+                                <div class="line-top">
+                                    <img class="selfprice-img" src="<?= SERVER_URL ?>assets/images/icons/selfprice.svg" alt="selfprice">
+                                    <p>Собівартість</p>
+                                </div>
+
+                                <div class="line-bottom">
+                                    <h2>~</h2>
+                                    <h3>37</h3>
+                                    <div class="fraction">
+                                        <p>грн</p>
+                                        <hr class="black">
+                                        <p>річ</p>
+                                    </div>
+                                </div>
+
+                            </div>
+                        </div>
+
+                        <div class="camera-line">
+                            <img src="<?= SERVER_URL ?>assets/images/icons/camera.svg" alt="camera">
+                            <p>Усі фото є оригінальними</p>
+                        </div>
+
+                    </div>
+                </div>
+                <div class="bottom-part">
+                    <img src="<?= SERVER_URL ?>assets/images/product/product-2.png" alt="product-2">
+                    <form class="order">
+
+                        <h2>Замовити</h2>
+
+                        <div class="name-field">
+                            <label for="name"></label>
+                            <input placeholder="ВАШЕ ІМ'Я" type="text" id="name" name="name" minlength="2">
+                        </div>
+
+                        <div class="number-field">
+                            <label for="number"></label>
+                            <input placeholder="ВАШ НОМЕР ТЕЛЕФОНУ" type="number" id="number" name="number" min="10" max="100">
+                        </div>
+
+                        <div class="count-field">
+                            <label for="count"></label>
+                            <input placeholder="Кількість (кг)" type="number" id="count" name="count" min="1" max="99">
+                        </div>
+
+                        <button class="send" type="submit" value="Submit" onclick="return false;">
+                            Надіслати
+                        </button>
+
+                    </form>
+                </div>
+            </div>
+
+        </div>
+
+        <div class="container">
+            <div class="marketing-section">
+
+                <div class="top-line">
+                    <hr class="red">
+                    <h2>ТАКОЖ ВАС МОЖЕ ЗАЦІКАВИТИ</h2>
+                    <hr class="red">
+                </div>
+
+                <div class="marketing-wrap">
+                    <div class="sell-box">
+
+                        <div class="image-bg"></div>
+
+                        <div class="content-wrap">
+
+                            <div class="content-block">
+
+                                <div class="top-part">
+
+                                    <div class="label">ВЗУТТЯ</div>
+
+                                    <h3>Речі для дому!</h3>
+
+                                    <hr class="orange bot">
+
+                                    <p>Собівартість одної речі</p>
+
+                                </div>
+
+                                <div class="bottom-part">
+                                    <h4>65,90 <span>$</span></h4>
+
+                                    <button class="specify">
+                                        <span>Вибрати</span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="8" viewBox="0 0 30 8" fill="none">
+                                            <path d="M0.09375 3.90618H28.6416M28.6416 3.90618L24.6207 0.553375M28.6416 3.90618L24.6207 7.25899" stroke="#F1AE2C"/>
+                                        </svg>
+                                    </button>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div class="sell-box">
+
+                        <div class="image-bg"></div>
+
+                        <div class="content-wrap">
+
+                            <div class="content-block">
+
+                                <div class="top-part">
+
+                                    <div class="label">ВЗУТТЯ</div>
+
+                                    <h3>Речі для дому!</h3>
+
+                                    <hr class="orange bot">
+
+                                    <p>Собівартість одної речі</p>
+
+                                </div>
+
+                                <div class="bottom-part">
+                                    <h4>65,90 <span>$</span></h4>
+
+                                    <button class="specify">
+                                        <span>Вибрати</span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="8" viewBox="0 0 30 8" fill="none">
+                                            <path d="M0.09375 3.90618H28.6416M28.6416 3.90618L24.6207 0.553375M28.6416 3.90618L24.6207 7.25899" stroke="#F1AE2C"/>
+                                        </svg>
+                                    </button>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div class="sell-box">
+
+                        <div class="image-bg"></div>
+
+                        <div class="content-wrap">
+
+                            <div class="content-block">
+
+                                <div class="top-part">
+
+                                    <div class="label">ВЗУТТЯ</div>
+
+                                    <h3>Речі для дому!</h3>
+
+                                    <hr class="orange bot">
+
+                                    <p>Собівартість одної речі</p>
+
+                                </div>
+
+                                <div class="bottom-part">
+                                    <h4>65,90 <span>$</span></h4>
+
+                                    <button class="specify">
+                                        <span>Вибрати</span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="8" viewBox="0 0 30 8" fill="none">
+                                            <path d="M0.09375 3.90618H28.6416M28.6416 3.90618L24.6207 0.553375M28.6416 3.90618L24.6207 7.25899" stroke="#F1AE2C"/>
+                                        </svg>
+                                    </button>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                    <div class="sell-box">
+
+                        <div class="image-bg"></div>
+
+                        <div class="content-wrap">
+
+                            <div class="content-block">
+
+                                <div class="top-part">
+
+                                    <div class="label">ВЗУТТЯ</div>
+
+                                    <h3>Речі для дому!</h3>
+
+                                    <hr class="orange bot">
+
+                                    <p>Собівартість одної речі</p>
+
+                                </div>
+
+                                <div class="bottom-part">
+                                    <h4>65,90 <span>$</span></h4>
+
+                                    <button class="specify">
+                                        <span>Вибрати</span>
+                                        <svg xmlns="http://www.w3.org/2000/svg" width="30" height="8" viewBox="0 0 30 8" fill="none">
+                                            <path d="M0.09375 3.90618H28.6416M28.6416 3.90618L24.6207 0.553375M28.6416 3.90618L24.6207 7.25899" stroke="#F1AE2C"/>
+                                        </svg>
+                                    </button>
+                                </div>
+
+                            </div>
+
+                        </div>
+
+                    </div>
+                </div>
+
+                <div class="margin">
+                    <button class="see-more">
+                        <span>Усі товари</span>
+                        <svg xmlns="http://www.w3.org/2000/svg" width="53" height="12" viewBox="0 0 53 12" fill="none">
+                            <path d="M0 6H51.5M51.5 6L46.5 1M51.5 6L46.5 11" stroke="#F1AE2C"/>
+                        </svg>
+                    </button>
+                </div>
+
+            </div>
         </div>
 
     </div>
